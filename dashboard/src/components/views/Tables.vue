0e64d7aa6da75690c6aa6882f52954d3221c2862
Oliver Putzer <emea@spamtrawler.net>
2019-05-03 06:50:51 +0200

    - Added datatables to frontend - Dynamic controller/routes/repository

---
+++ dashboard/src/components/views/Tables.vue
@@ -0,0 +1,167 @@
+<template>
+  <section class="content">
+    <div class="row center-block">
+      <h2>Simple</h2>
+      <div class="col-md-12">
+        <div class="box">
+          <div class="box-header">
+            <h3 class="box-title">Striped Full Width Table</h3>
+          </div>
+          <!-- /.box-header -->
+          <div class="box-body no-padding table-responsive">
+            <table class="table table-striped">
+              <tbody>
+                <tr>
+                  <th style="width: 10px">#</th>
+                  <th>Task</th>
+                  <th>Progress</th>
+                  <th style="width: 40px">Label</th>
+                </tr>
+                <tr>
+                  <td>1.</td>
+                  <td>Update software</td>
+                  <td>
+                    <div class="progress progress-xs">
+                      <div class="progress-bar progress-bar-danger" style="width: 55%"></div>
+                    </div>
+                  </td>
+                  <td><span class="badge bg-red">55%</span></td>
+                </tr>
+                <tr>
+                  <td>2.</td>
+                  <td>Clean database</td>
+                  <td>
+                    <div class="progress progress-xs">
+                      <div class="progress-bar progress-bar-yellow" style="width: 70%"></div>
+                    </div>
+                  </td>
+                  <td><span class="badge bg-yellow">70%</span></td>
+                </tr>
+                <tr>
+                  <td>3.</td>
+                  <td>Cron job running</td>
+                  <td>
+                    <div class="progress progress-xs progress-striped active">
+                      <div class="progress-bar progress-bar-primary" style="width: 30%"></div>
+                    </div>
+                  </td>
+                  <td><span class="badge bg-light-blue">30%</span></td>
+                </tr>
+                <tr>
+                  <td>4.</td>
+                  <td>Fix and squish bugs</td>
+                  <td>
+                    <div class="progress progress-xs progress-striped active">
+                      <div class="progress-bar progress-bar-success" style="width: 90%"></div>
+                    </div>
+                  </td>
+                  <td><span class="badge bg-green">90%</span></td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <!-- /.box-body -->
+        </div>
+      </div>
+    </div>
+
+    <div class="row center-block">
+      <h2>Data tables</h2>
+      <div class="col-md-12">
+        <div class="box">
+          <div class="box-header">
+            <h3 class="box-title">Data Table With Full Features</h3>
+          </div>
+          <!-- /.box-header -->
+          <div class="box-body">
+            <div class="dataTables_wrapper form-inline dt-bootstrap" id="example1_wrapper">
+              <div class="row">
+                <div class="col-sm-6">
+                  <div id="example1_length" class="dataTables_length">
+
+                  </div>
+                </div>
+              </div>
+
+              <div class="row">
+                <div class="col-sm-12 table-responsive">
+                  <table aria-describedby="example1_info" role="grid" id="example1" class="table table-bordered table-striped dataTable">
+                    <thead>
+                    <tr>
+                      <th>Value</th>
+                      <th>Status</th>
+                    </tr>
+                    </thead>
+                    <tfoot>
+                    <tr>
+                      <th>Value</th>
+                      <th>Status</th>
+                    </tr>
+                    </tfoot>
+                  </table>
+                </div>
+              </div>
+            </div>
+            <!-- /.box-body -->
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import $ from 'jquery'
+// Require needed datatables modules
+require('datatables.net')
+require('datatables.net-bs')
+
+export default {
+  name: 'Tables',
+  props: ['collection'],
+  mounted() {
+    this.$nextTick(() => {
+      $('#example1').DataTable(
+        {
+          'ajax': 'http://127.0.0.1:1232/manage/' + this.$route.params.collection + '/all',
+          'columns': [
+            { 'data': 'value' },
+            { 'data': 'status' }
+          ]
+        }
+      )
+    })
+  }
+}
+</script>
+
+<style>
+/* Using the bootstrap style, but overriding the font to not draw in
+   the Glyphicons Halflings font as an additional requirement for sorting icons.
+
+   An alternative to the solution active below is to use the jquery style
+   which uses images, but the color on the images does not match adminlte.
+
+@import url('/static/js/plugins/datatables/jquery.dataTables.min.css');
+*/
+
+@import url('/static/js/plugins/datatables/dataTables.bootstrap.css');
+
+table.dataTable thead .sorting:after,
+table.dataTable thead .sorting_asc:after,
+table.dataTable thead .sorting_desc:after {
+  font-family: 'FontAwesome';
+}
+
+table.dataTable thead .sorting:after {
+  content: '\f0dc';
+}
+
+table.dataTable thead .sorting_asc:after {
+  content: '\f0dd';
+}
+
+table.dataTable thead .sorting_desc:after {
+  content: '\f0de';
+}
+</style>
